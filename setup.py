c51244bb3e01738dafb52744451f7a059aab595b
jaredwines <jared.wines@pricefx.com>
2023-07-24 21:52:43 -0700

    j

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name="deploymenthub",
-    version=1.5,
+    version=1.6,
     description='A CLI client for deployment-hub server.',
     license='MIT',
     author='Jared Wines',
